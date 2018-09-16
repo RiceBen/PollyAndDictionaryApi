28147c78b9e28e74f762a85d2f541145cb417095
benjamin <benjamin750715@gmail.com>
2018-09-16 15:05:47 +0800

    Init Repo

---
+++ PollyAndDictionaryApi/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PollyAndDictionaryApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Query(string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
