e4a87c452a9f2d4c9ed3883f21c9941b324484ca
benjamin <benjamin750715@gmail.com>
2018-11-22 11:26:49 +0800

    Remove unused code

--- PollyAndDictionaryApi/Controllers/HomeController.cs
+++ PollyAndDictionaryApi/Controllers/HomeController.cs
@@ -5,15 +5,6 @@ namespace PollyAndDictionaryApi.Controllers
 {
     public class HomeController : Controller
     {
-        private AWSService awsService;
-
-        public HomeController()
-        {
-            this.awsService = new AWSService();
-        }
-
-        public AWSService Service { get { return awsService; } }
-
         [HttpGet]
         public ActionResult Index()
         {
